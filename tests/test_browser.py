3a3678310798375f56b99ca4c477bef394c46863
ooiuuii <al3060388206@gmail.com>
2026-02-14 23:05:32 +0800

    Update test_browser.py

--- tests/test_browser.py
+++ tests/test_browser.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 
 
-def test_open_google():
+def test_open_page():
     options = Options()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
@@ -12,6 +12,6 @@ def test_open_google():
         command_executor="http://selenium:4444/wd/hub",
         options=options
     )
-    driver.get("https://www.google.com")
-    assert "Google" in driver.title
+    driver.get("https://cn.bing.com")
+    assert "Bing" in driver.title
     driver.quit()
